02e9760035c52099888e4cb7d2f75021f8bab8a2
Anne Castrillon <anne.castrillon@gmail.com>
2024-09-15 20:46:59 -0700

    Initial Commit

---
+++ NDS Plus/NDS_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDS_PlusApp.swift
+//  NDS Plus
+//
+//  Created by Anne Castrillon on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct NDS_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
