584cf67cb8652af6d7acb5f3472c256dd905de3e
Anne Castrillon <anne.castrillon@gmail.com>
2024-09-19 00:04:03 -0700

    refactor code and add GameView

--- NDS Plus/ContentView.swift
+++ NDS Plus/ContentView.swift
@@ -18,14 +18,14 @@ struct ContentView: View {
     @State private var bios9Data: Data?
     @State private var firmwareData: Data?
     @State private var romData: Data? = nil
-    @State private var topImage: UIImage = UIImage()
-    @State private var bottomImage: UIImage = UIImage()
     
     @State private var workItem: DispatchWorkItem? = nil
     @State private var isRunning = false
     @State private var loggedInCloud = false
+    
+    @State private var path = NavigationPath()
+    @State private var emulator: MobileEmulator? = nil
 
-    @State private var gameController = GameController()
     @Environment(\.modelContext) private var context
     
     @Query private var games: [Game] = []
@@ -66,11 +66,7 @@ struct ContentView: View {
             }
         }
     }
-    
-    let graphicsParser = GraphicsParser()
-    
-    @State private var emulator: MobileEmulator? = nil
-    
+        
     let ndsType = UTType(filenameExtension: "nds", conformingTo: .data)
     
     var buttonDisabled: Bool {
@@ -88,270 +84,123 @@ struct ContentView: View {
         }
     }
     
-    private func run() {
-        let bios7Arr: [UInt8] = Array(bios7Data!)
-        let bios9Arr: [UInt8] = Array(bios9Data!)
-        let firmwareArr: [UInt8] = Array(firmwareData!)
-        let romArr: [UInt8] = Array(romData!)
-        
-        var bios7Ptr: UnsafeBufferPointer<UInt8>? = nil
-        var bios9Ptr: UnsafeBufferPointer<UInt8>? = nil
-        var firmwarePtr: UnsafeBufferPointer<UInt8>? = nil
-        var romPtr: UnsafeBufferPointer<UInt8>? = nil
-        
-        bios7Arr.withUnsafeBufferPointer() { ptr in
-            bios7Ptr = ptr
-        }
-        
-        bios9Arr.withUnsafeBufferPointer() { ptr in
-            bios9Ptr = ptr
-        }
-        
-        firmwareArr.withUnsafeBufferPointer() { ptr in
-            firmwarePtr = ptr
-        }
-        romArr.withUnsafeBufferPointer() { ptr in
-            romPtr = ptr
-        }
-        emulator = MobileEmulator(
-            bios7Ptr!,
-            bios9Ptr!,
-            firmwarePtr!,
-            romPtr!
-        )
-        
-        isRunning = true
-        
-        workItem = DispatchWorkItem {
-            if let emu = emulator {
-                while true {
-                    DispatchQueue.main.sync {
-                        emu.step_frame()
-                        
-                        let aPixels = emu.get_engine_a_picture_pointer()
-                        
-                        var imageA = UIImage()
-                        var imageB = UIImage()
-                        
-                        if let image = graphicsParser.fromPointer(ptr: aPixels) {
-                            imageA = image
-                        }
-                        
-                        let bPixels = emu.get_engine_b_picture_pointer()
-                        
-                        if let image = graphicsParser.fromPointer(ptr: bPixels) {
-                            imageB = image
-                            
-                        }
-                        
-                        if emu.is_top_a() {
-                            topImage = imageA
-                            bottomImage = imageB
-                        } else {
-                            topImage = imageB
-                            bottomImage = imageA
-                        }
-                        
-                        self.handleInput()
-                    }
-                    
-                    if !isRunning {
-                        break
-                    }
-                }
-            }
-            
-        }
-        
-        DispatchQueue.global().async(execute: workItem!)
-    }
-    
-    private func handleInput() {
-        if let controller = self.gameController.controller.extendedGamepad {
-            if let emu = emulator {
-                emu.update_input(ButtonEvent.ButtonA, controller.buttonA.isPressed)
-                emu.update_input(ButtonEvent.ButtonB, controller.buttonB.isPressed)
-                emu.update_input(ButtonEvent.ButtonY, controller.buttonY.isPressed)
-                emu.update_input(ButtonEvent.ButtonX, controller.buttonX.isPressed)
-                emu.update_input(ButtonEvent.ButtonL, controller.leftShoulder.isPressed)
-                emu.update_input(ButtonEvent.ButtonR, controller.rightShoulder.isPressed)
-                emu.update_input(ButtonEvent.Start, controller.buttonMenu.isPressed)
-                emu.update_input(
-                    ButtonEvent.Select,
-                    controller.buttonOptions?.isPressed ?? false
-                )
-                emu.update_input(ButtonEvent.Up, controller.dpad.up.isPressed)
-                emu.update_input(ButtonEvent.Down, controller.dpad.down.isPressed)
-                emu.update_input(ButtonEvent.Left, controller.dpad.left.isPressed)
-                emu.update_input(ButtonEvent.Right, controller.dpad.right.isPressed)
-            }
-            
-        }
-    }
-    
     @Environment(\.colorScheme) var colorScheme
     var body: some View {
-        VStack {
-            HStack {
+        NavigationStack(path: $path) {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button("", systemImage: "gear") {
+                        showSettings.toggle()
+                    }
+                    .font(.title)
+                    .frame(alignment: .trailing)
+                    .foregroundColor(.indigo)
+                    .padding(.trailing)
+                    
+                }
+                .sheet(isPresented: $showSettings) {
+                    VStack {
+                        SettingsView(
+                            bios7Data: $bios7Data,
+                            bios9Data: $bios9Data,
+                            firmwareData: $firmwareData,
+                            loggedInCloud: $loggedInCloud
+                        )
+                    }
+                    .background(colorScheme == .dark ? Color.black : Color.white)
+                }
+                HStack {
+                    Text("NDS Plus")
+                        .font(.largeTitle)
+                        .foregroundColor(colorScheme == .dark ? .white : .black)
+                        .frame(alignment: .center)
+                }
                 Spacer()
-                Button("", systemImage: "gear") {
-                    showSettings.toggle()
-                }
-                .font(.title)
-                .frame(alignment: .trailing)
-                .foregroundColor(.indigo)
-                .padding(.trailing)
-
-            }
-            .sheet(isPresented: $showSettings) {
-                VStack {
-                    SettingsView(
-                        bios7Data: $bios7Data,
-                        bios9Data: $bios9Data,
-                        firmwareData: $firmwareData,
-                        loggedInCloud: $loggedInCloud
-                    )
-                }
-                .background(colorScheme == .dark ? Color.black : Color.white)
-            }
-            HStack {
-                Text("NDS Plus")
-                    .font(.largeTitle)
-                    .foregroundColor(colorScheme == .dark ? .white : .black)
-                    .frame(alignment: .center)
-            }
-            // Spacer()
-//            Image(uiImage: topImage)
-//                .resizable()
-//                .frame(
-//                    width: CGFloat(SCREEN_WIDTH) * 1.25,
-//                    height: CGFloat(SCREEN_HEIGHT) * 1.25
-//                )
-//            Image(uiImage: bottomImage)
-//                .resizable()
-//                .frame(
-//                    width: CGFloat(SCREEN_WIDTH) * 1.25,
-//                    height: CGFloat(SCREEN_HEIGHT) * 1.25
-//                )
-//                .onTapGesture() { location in
-//                    if location.x >= 0 && location.y >= 0 {
-//                        let x = UInt16(Float(location.x) / 1.25)
-//                        let y = UInt16(Float(location.y) / 1.25)
-//                    
-//                        emulator?.touch_screen(x, y)
-//                        
-//                        DispatchQueue.global().async(execute: DispatchWorkItem {
-//                            usleep(200)
-//                            DispatchQueue.main.sync() {
-//                                emulator?.release_screen()
-//                            }
-//                        })
-//                    }
-//                   
-//                }
-//                .simultaneousGesture(
-//                    DragGesture(minimumDistance: 0)
-//                        .onChanged() { value in
-//                            if value.location.x >= 0 && value.location.y >= 0 {
-//                                let x = UInt16(Float(value.location.x) / 1.25)
-//                                let y = UInt16(Float(value.location.y) / 1.25)
-//                                emulator?.touch_screen(x, y)
-//                            }
-//                        }
-//                        .onEnded() { value in
-//                            if value.location.x >= 0 && value.location.y >= 0 {
-//                                let x = UInt16(Float(value.location.x) / 1.25)
-//                                let y = UInt16(Float(value.location.y) / 1.25)
-//                                emulator?.touch_screen(x, y)
-//                                DispatchQueue.global().async(execute: DispatchWorkItem {
-//                                    usleep(200)
-//                                    DispatchQueue.main.sync() {
-//                                        emulator?.release_screen()
-//                                    }
-//                                })
-//                            }
-//                            
-//                        }
-//                )
-//                
-//            Spacer()
-            Spacer()
-            if games.count > 0 {
-                List {
-                    Section(header: Text("Games")) {
-                        ForEach(games) { game in
-                            HStack {
-                                Button(game.gameName.removingPercentEncoding!) {
-                                    if let data = try? Data(contentsOf: game.path) {
-                                        romData = data
-                                        
-                                        if bios7Data != nil && 
-                                            bios9Data != nil &&
-                                            firmwareData != nil
-                                        {
-                                            self.run()
+                if games.count > 0 {
+                    List {
+                        Section(header: Text("Games")) {
+                            ForEach(games) { game in
+                                HStack {
+                                    Button(game.gameName.removingPercentEncoding!) {
+                                        if let data = try? Data(contentsOf: game.path) {
+                                            romData = data
+                                            
+                                            if bios7Data != nil &&
+                                                bios9Data != nil &&
+                                                firmwareData != nil
+                                            {
+                                                path.append("GameView")
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                } else {
+                    Spacer()
+                    Spacer()
                 }
-            } else {
-                Spacer()
-                Spacer()
-            }
-            HStack {
-                Button("Load Game", systemImage: "square.and.arrow.up.circle") {
-                    emulator = nil
-                    workItem?.cancel()
-                    isRunning = false
-                    
-                    workItem = nil
-                    
-                    showRomDialog = true
-                }
-                .foregroundColor(buttonColor)
-                .disabled(buttonDisabled)
-                .font(.title)
-            }
-            Spacer()
-        }
-        .fileImporter(
-            isPresented: $showRomDialog,
-            allowedContentTypes: [ndsType.unsafelyUnwrapped]
-        ) { result in
-            if let url = try? result.get() {
-                if url.startAccessingSecurityScopedResource() {
-                    defer {
-                        url.stopAccessingSecurityScopedResource()
-                    }
-                    if let data = try? Data(contentsOf: url) {
-
-                        romData = data
+                HStack {
+                    Button("Load Game", systemImage: "square.and.arrow.up.circle") {
+                        emulator = nil
+                        workItem?.cancel()
+                        isRunning = false
                         
-                        if bios7Data != nil && bios9Data != nil && firmwareData != nil {
-                            self.run()
+                        workItem = nil
+                        
+                        showRomDialog = true
+                    }
+                    .foregroundColor(buttonColor)
+                    .disabled(buttonDisabled)
+                    .font(.title)
+                }
+            }
+            .fileImporter(
+                isPresented: $showRomDialog,
+                allowedContentTypes: [ndsType.unsafelyUnwrapped]
+            ) { result in
+                if let url = try? result.get() {
+                    if url.startAccessingSecurityScopedResource() {
+                        defer {
+                            url.stopAccessingSecurityScopedResource()
+                        }
+                        if let data = try? Data(contentsOf: url) {
                             
-                            // finally store the game in local db for use later.
-                            let game = Game(
-                                path: url,
-                                gameName: String(url
-                                    .relativeString
-                                    .split(separator: "/")
-                                    .last
-                                    .unsafelyUnwrapped
+                            romData = data
+                            
+                            if bios7Data != nil && bios9Data != nil && firmwareData != nil {
+                                let game = Game(
+                                    path: url,
+                                    gameName: String(url
+                                        .relativeString
+                                        .split(separator: "/")
+                                        .last
+                                        .unsafelyUnwrapped
+                                    )
+                                    .removingPercentEncoding!
                                 )
-                                .removingPercentEncoding!
-                            )
-                            
-                            context.insert(game)
+                                context.insert(game)
+                                path.append("GameView")
+                            }
                         }
                     }
                 }
             }
+            .background(colorScheme == .dark ? Color.black : Color.white )
+            .navigationDestination(for: String.self) { view in
+                if view == "GameView" {
+                    GameView(
+                        emulator: $emulator,
+                        bios7Data: $bios7Data,
+                        bios9Data: $bios9Data,
+                        firmwareData: $firmwareData,
+                        romData: $romData
+                    )
+                }
+            }
         }
-        .background(colorScheme == .dark ? Color.black : Color.white )
     }
 }
 
---
+++ NDS Plus/GameView.swift
@@ -0,0 +1,197 @@
+//
+//  GameView.swift
+//  NDS Plus
+//
+//  Created by Anne Castrillon on 9/18/24.
+//
+
+import SwiftUI
+import DSEmulatorMobile
+
+struct GameView: View {
+    @State private var topImage = UIImage()
+    @State private var bottomImage = UIImage()
+    @Binding var emulator: MobileEmulator?
+    
+    @State private var isRunning = false
+    
+    @Binding var bios7Data: Data?
+    @Binding var bios9Data: Data?
+    @Binding var firmwareData: Data?
+    @Binding var romData: Data?
+    
+    @State private var gameController = GameController()
+    
+    @State private var workItem: DispatchWorkItem? = nil
+    
+    private let graphicsParser = GraphicsParser()
+    
+    private func run() {
+        let bios7Arr: [UInt8] = Array(bios7Data!)
+        let bios9Arr: [UInt8] = Array(bios9Data!)
+        let firmwareArr: [UInt8] = Array(firmwareData!)
+        let romArr: [UInt8] = Array(romData!)
+        
+        var bios7Ptr: UnsafeBufferPointer<UInt8>? = nil
+        var bios9Ptr: UnsafeBufferPointer<UInt8>? = nil
+        var firmwarePtr: UnsafeBufferPointer<UInt8>? = nil
+        var romPtr: UnsafeBufferPointer<UInt8>? = nil
+        
+        bios7Arr.withUnsafeBufferPointer() { ptr in
+            bios7Ptr = ptr
+        }
+        
+        bios9Arr.withUnsafeBufferPointer() { ptr in
+            bios9Ptr = ptr
+        }
+        
+        firmwareArr.withUnsafeBufferPointer() { ptr in
+            firmwarePtr = ptr
+        }
+        romArr.withUnsafeBufferPointer() { ptr in
+            romPtr = ptr
+        }
+        emulator = MobileEmulator(
+            bios7Ptr!,
+            bios9Ptr!,
+            firmwarePtr!,
+            romPtr!
+        )
+        
+        isRunning = true
+        
+        workItem = DispatchWorkItem {
+            if let emu = emulator {
+                while true {
+                    DispatchQueue.main.sync {
+                        emu.step_frame()
+                        
+                        let aPixels = emu.get_engine_a_picture_pointer()
+                        
+                        var imageA = UIImage()
+                        var imageB = UIImage()
+                        
+                        if let image = graphicsParser.fromPointer(ptr: aPixels) {
+                            imageA = image
+                        }
+                        
+                        let bPixels = emu.get_engine_b_picture_pointer()
+                        
+                        if let image = graphicsParser.fromPointer(ptr: bPixels) {
+                            imageB = image
+                            
+                        }
+                        
+                        if emu.is_top_a() {
+                            topImage = imageA
+                            bottomImage = imageB
+                        } else {
+                            topImage = imageB
+                            bottomImage = imageA
+                        }
+                        
+                        self.handleInput()
+                    }
+                    
+                    if !isRunning {
+                        break
+                    }
+                }
+            }
+            
+        }
+        
+        DispatchQueue.global().async(execute: workItem!)
+    }
+    
+    private func handleInput() {
+        if let controller = self.gameController.controller.extendedGamepad {
+            if let emu = emulator {
+                emu.update_input(ButtonEvent.ButtonA, controller.buttonA.isPressed)
+                emu.update_input(ButtonEvent.ButtonB, controller.buttonB.isPressed)
+                emu.update_input(ButtonEvent.ButtonY, controller.buttonY.isPressed)
+                emu.update_input(ButtonEvent.ButtonX, controller.buttonX.isPressed)
+                emu.update_input(ButtonEvent.ButtonL, controller.leftShoulder.isPressed)
+                emu.update_input(ButtonEvent.ButtonR, controller.rightShoulder.isPressed)
+                emu.update_input(ButtonEvent.Start, controller.buttonMenu.isPressed)
+                emu.update_input(
+                    ButtonEvent.Select,
+                    controller.buttonOptions?.isPressed ?? false
+                )
+                emu.update_input(ButtonEvent.Up, controller.dpad.up.isPressed)
+                emu.update_input(ButtonEvent.Down, controller.dpad.down.isPressed)
+                emu.update_input(ButtonEvent.Left, controller.dpad.left.isPressed)
+                emu.update_input(ButtonEvent.Right, controller.dpad.right.isPressed)
+            }
+            
+        }
+    }
+    var body: some View {
+        ZStack {
+            Color.pink
+            VStack {
+                Spacer()
+                Image(uiImage: topImage)
+                    .resizable()
+                    .frame(
+                        width: CGFloat(SCREEN_WIDTH) * 1.5,
+                        height: CGFloat(SCREEN_HEIGHT) * 1.5
+                    )
+                Image(uiImage: bottomImage)
+                    .resizable()
+                    .frame(
+                        width: CGFloat(SCREEN_WIDTH) * 1.5,
+                        height: CGFloat(SCREEN_HEIGHT) * 1.5
+                    )
+                    .onTapGesture() { location in
+                        if location.x >= 0 && location.y >= 0 {
+                            let x = UInt16(Float(location.x) / 1.5)
+                            let y = UInt16(Float(location.y) / 1.5)
+                            
+                            emulator?.touch_screen(x, y)
+                            
+                            DispatchQueue.global().async(execute: DispatchWorkItem {
+                                usleep(200)
+                                DispatchQueue.main.sync() {
+                                    emulator?.release_screen()
+                                }
+                            })
+                        }
+                        
+                    }
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged() { value in
+                                if value.location.x >= 0 && value.location.y >= 0 {
+                                    let x = UInt16(Float(value.location.x) / 1.5)
+                                    let y = UInt16(Float(value.location.y) / 1.5)
+                                    emulator?.touch_screen(x, y)
+                                }
+                            }
+                            .onEnded() { value in
+                                if value.location.x >= 0 && value.location.y >= 0 {
+                                    let x = UInt16(Float(value.location.x) / 1.5)
+                                    let y = UInt16(Float(value.location.y) / 1.5)
+                                    emulator?.touch_screen(x, y)
+                                    DispatchQueue.global().async(execute: DispatchWorkItem {
+                                        usleep(200)
+                                        DispatchQueue.main.sync() {
+                                            emulator?.release_screen()
+                                        }
+                                    })
+                                }
+                                
+                            }
+                    )
+                Spacer()
+                Spacer()
+            }
+        }
+        .onAppear {
+            self.run()
+        }
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
