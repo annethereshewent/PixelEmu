221853675f78e3f979b4b6c46e08cadb4f712abb
Anne Castrillon <anne.castrillon@gmail.com>
2024-10-18 17:53:53 -0700

    minor changes

--- NDS Plus/views/main/GameEntryView.swift
+++ NDS Plus/views/main/GameEntryView.swift
@@ -15,19 +15,23 @@ struct GameEntryView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            ZStack {
-                Image("Cartridge")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                VStack {
-                    if let image = graphicsParser.fromBytes(bytes: game.gameIcon, width: 32, height: 32) {
-                        let uiImage = UIImage(cgImage: image)
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .frame(width: 64, height: 64)
+            Button {
+                callback()
+            } label: {
+                ZStack {
+                    Image("Cartridge")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                    VStack {
+                        if let image = graphicsParser.fromBytes(bytes: game.gameIcon, width: 32, height: 32) {
+                            let uiImage = UIImage(cgImage: image)
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .frame(width: 64, height: 64)
+                        }
                     }
+                    Spacer()
                 }
-                Spacer()
             }
             Text(game.gameName.replacing(".nds", with: ""))
                 .frame(width: 80, height: 80)
