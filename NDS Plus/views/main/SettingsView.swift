c7b835f9c28a4a0e7a2512df6d411e4b04c2a37b
Anne Castrillon <anne.castrillon@gmail.com>
2024-10-19 10:47:58 -0700

    (hopefully) fix a bug where path.append would be called twice

--- NDS Plus/views/main/SettingsView.swift
+++ NDS Plus/views/main/SettingsView.swift
@@ -54,121 +54,119 @@ struct SettingsView: View {
     }
 
     var body: some View {
-        NavigationStack(path: $path) {
-            VStack {
-                HStack {
-                    Text("NDS+ Settings")
-                        .font(.custom("Departure Mono", size: 28))
-                        .foregroundColor(Colors.primaryColor)
-                        .fontWeight(.bold)
-                        .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
-                }
-                VStack {
-                    Text("Optional binary files")
-                        .padding(.bottom, 20)
-                    HStack {
-                        Button("Bios 7") {
-                            showFileBrowser = true
-                            currentFile = CurrentFile.bios7
-                        }
-                        Spacer()
-                        if bios7Loaded {
-                            Image(systemName: "checkmark")
-                                .foregroundColor(.green)
-                        }
-                    }
-                    HStack {
-                        Button("Bios 9") {
-                            showFileBrowser = true
-                            currentFile = CurrentFile.bios9
-                        }
-                        Spacer()
-                        if bios9Loaded {
-                            Image(systemName: "checkmark")
-                                .foregroundColor(.green)
-                        }
-                    }
-                    
-                    HStack {
-                        Button("Firmware") {
-                            showFileBrowser = true
-                            currentFile = CurrentFile.firmware
-                        }
-                        Spacer()
-                        if firmwareData != nil {
-                            Image(systemName: "checkmark")
-                                .foregroundColor(.green)
-                        }
-                    }
-      
-                    ColorPicker("Skin theme color", selection: $themeColor)
-                    
-                    .padding(.top, 20)
-                    .foregroundColor(Colors.accentColor)
-                    
-                    Toggle(isOn: $isSoundOn) {
-                        Text("Start game with sound")
-                    }
-                    .toggleStyle(.switch)
-                    .padding(.top, 20)
-                    
-                    Button {
-                        if let url = URL(string: "https://www.github.com/annethereshewent") {
-                            UIApplication.shared.open(url)
-                        }
-                    } label: {
-                        Image("Github")
-                            .padding(.top, 60)
-                    }
-                }
-                .frame(width: 400, height: 600)
-                Spacer()
+        VStack {
+            HStack {
+                Text("NDS+ Settings")
+                    .font(.custom("Departure Mono", size: 28))
+                    .foregroundColor(Colors.primaryColor)
+                    .fontWeight(.bold)
+                    .foregroundColor(colorScheme == .dark ? Color.white : Color.black)
             }
-            .font(.custom("Departure Mono", size: 24))
-            .foregroundColor(Colors.primaryColor)
-            .fileImporter(
-                isPresented: $showFileBrowser,
-                allowedContentTypes: [binType.unsafelyUnwrapped]
-            ) { result in
-                if let url = try? result.get() {
-                    if url.startAccessingSecurityScopedResource() {
-                        defer {
-                            url.stopAccessingSecurityScopedResource()
-                        }
-                        if let data = try? Data(contentsOf: url) {
-                            if let file = currentFile {
-                                switch file {
-                                    // store the file in application support
-                                case .bios7:
-                                    bios7Data = data
-                                case .bios9:
-                                    bios9Data = data
-                                case .firmware:
-                                    firmwareData = data
-                                }
-                                if let location = try? FileManager.default.url(
-                                    for: .applicationSupportDirectory,
-                                    in: .userDomainMask,
-                                    appropriateFor: nil,
-                                    create: true
-                                ) {
-                                    self.storeFile(location: location, data: data, currentFile: file)
-                                }
+            VStack {
+                Text("Optional binary files")
+                    .padding(.bottom, 20)
+                HStack {
+                    Button("Bios 7") {
+                        showFileBrowser = true
+                        currentFile = CurrentFile.bios7
+                    }
+                    Spacer()
+                    if bios7Loaded {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                    }
+                }
+                HStack {
+                    Button("Bios 9") {
+                        showFileBrowser = true
+                        currentFile = CurrentFile.bios9
+                    }
+                    Spacer()
+                    if bios9Loaded {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                    }
+                }
+                
+                HStack {
+                    Button("Firmware") {
+                        showFileBrowser = true
+                        currentFile = CurrentFile.firmware
+                    }
+                    Spacer()
+                    if firmwareData != nil {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.green)
+                    }
+                }
+  
+                ColorPicker("Skin theme color", selection: $themeColor)
+                
+                .padding(.top, 20)
+                .foregroundColor(Colors.accentColor)
+                
+                Toggle(isOn: $isSoundOn) {
+                    Text("Start game with sound")
+                }
+                .toggleStyle(.switch)
+                .padding(.top, 20)
+                
+                Button {
+                    if let url = URL(string: "https://www.github.com/annethereshewent") {
+                        UIApplication.shared.open(url)
+                    }
+                } label: {
+                    Image("Github")
+                        .padding(.top, 60)
+                }
+            }
+            .frame(width: 400, height: 600)
+            Spacer()
+        }
+        .font(.custom("Departure Mono", size: 24))
+        .foregroundColor(Colors.primaryColor)
+        .fileImporter(
+            isPresented: $showFileBrowser,
+            allowedContentTypes: [binType.unsafelyUnwrapped]
+        ) { result in
+            if let url = try? result.get() {
+                if url.startAccessingSecurityScopedResource() {
+                    defer {
+                        url.stopAccessingSecurityScopedResource()
+                    }
+                    if let data = try? Data(contentsOf: url) {
+                        if let file = currentFile {
+                            switch file {
+                                // store the file in application support
+                            case .bios7:
+                                bios7Data = data
+                            case .bios9:
+                                bios9Data = data
+                            case .firmware:
+                                firmwareData = data
+                            }
+                            if let location = try? FileManager.default.url(
+                                for: .applicationSupportDirectory,
+                                in: .userDomainMask,
+                                appropriateFor: nil,
+                                create: true
+                            ) {
+                                self.storeFile(location: location, data: data, currentFile: file)
                             }
                         }
                     }
-                    
                 }
                 
-                if bios7Data != nil && bios9Data != nil && firmwareData != nil {
-                    dismiss()
-                }
             }
-            .onChange(of: isSoundOn) {
-                let defaults = UserDefaults.standard
-                
-                defaults.setValue(isSoundOn, forKey: "isSoundOn")
+            
+            if bios7Data != nil && bios9Data != nil && firmwareData != nil {
+                dismiss()
             }
+        }
+        .onChange(of: isSoundOn) {
+            let defaults = UserDefaults.standard
+            
+            defaults.setValue(isSoundOn, forKey: "isSoundOn")
         }
     }
 }
